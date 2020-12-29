b8e0333289652222a60ded56134f3207bcc602ec
Sujee <sathasivam.sujee@gmail.com>
2020-12-29 16:09:01 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+"use strict";
+const express = require("express");
+const app = express();
+const port = process.env.port || 5000;
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    //handle root
+    res.send("hi root");
+});
+
+app.listen(port, err => {
+    if (err) return console.log("ERROR", err);
+    console.log(`Listening on port ${port}`);
+});
